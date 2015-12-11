88af8325ae382736b6b685a48d9750edc0a40b1c
xiangkundai <xiangkundai@gmail.com>
2015-12-10 23:54:04 -0600

    viewEvent fixed

--- app/src/main/java/com/application/Kevin_Wenwen/FreeLunch/DisplayOneEvent.java
+++ app/src/main/java/com/application/Kevin_Wenwen/FreeLunch/DisplayOneEvent.java
@@ -38,7 +38,7 @@ import java.util.ArrayList;
  * Created by wenwen on 10/21/15.
  */
 public class DisplayOneEvent extends ActionBarActivity {
-    private String TAG  = "Display Subscribes";
+    private String TAG  = "Display one event";
     public final static String EXTRA_MESSAGE = "MESSAGE IN";
     // to mark which image has been displayed
     private int building = 0;
@@ -64,10 +64,13 @@ public class DisplayOneEvent extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_one_event);
 
+        Intent intent = getIntent();
+        msg = intent.getStringArrayExtra(EXTRA_MESSAGE);
 
         final String request_url = "http://freelunch-test1.appspot.com/ViewOneEvent";
         AsyncHttpClient httpClient = new AsyncHttpClient();
         RequestParams params = new RequestParams();
+        eventName = msg[1];
         params.put("event_name", eventName);
 
         httpClient.get(request_url, params, new AsyncHttpResponseHandler() {
@@ -80,47 +83,51 @@ public class DisplayOneEvent extends ActionBarActivity {
                 final String des;
                 final String coverUrl;
                 final String link;
-                final String rating;
+                String rating;
                 final String author_name;
 
                 try {
+                    System.out.println("I'm getting into try");
                     JSONObject jObject = new JSONObject(new String(response));
 
                     dtStart = jObject.getString("dt_start");
+                    System.out.println("getting dt_start "+dtStart);
+
                     dtEnd = jObject.getString("dt_end");
                     building = jObject.getString("building");
                     room = jObject.getString("room");
                     des = jObject.getString("description");
                     coverUrl = jObject.getString("coverUrl");
+                    System.out.println("getting coverurl " + coverUrl);
                     link = jObject.getString("linkage");
                     rating = jObject.getString("ratings");
+
                     author_name = jObject.getString("author_name");
+                    System.out.println("getting author_name " + author_name);
 
-//                    if(email!=null) {
-//                        if (email.split("@",2)[0].equals(stream_author.toLowerCase()) ){
-//                        upload.setEnabled(true);
-//                     //   Log.d("Upload ability","OK!!");
-//                        } else {
-//                            upload.setEnabled(false);
-//                            //   Log.d("Upload ability", "not OK!!");
-//                        }
-//                    } else {
-//                        upload.setEnabled(false);
-//                     //   Log.d("Upload ability", "not OK!!");
-//                    }
-//
+
                     // adapters
-                    URL url = new URL(coverUrl);
-                    Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+//                    URL url = new URL(coverUrl);
+//                    Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
                     ImageView imgView = (ImageView) findViewById(R.id.imgview);
-                    imgView.setImageBitmap(bmp);
-
+//                    imgView.setImageBitmap(bmp);
+                    Picasso.with(context).load(coverUrl).into(imgView);
+                    System.out.println("Setting image");
+//
+                    if (rating == "null") {
+                        rating = "4.5";
+                    }
                     float ratingValue = Float.parseFloat(rating);
                     RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
                     ratingBar.setRating(ratingValue);
 
                     TextView t = (TextView) findViewById(R.id.dtStart);
+                    System.out.println("Found layout id dtStart");
                     t.setText(dtStart);
+                    System.out.println("wrote to dtStart");
+                    t = (TextView) findViewById(R.id.event_name);
+                    t.setText(eventName);
+
                     t = (TextView) findViewById(R.id.dtEnd);
                     t.setText(dtEnd);
                     t = (TextView) findViewById(R.id.building);
@@ -132,8 +139,8 @@ public class DisplayOneEvent extends ActionBarActivity {
                     t = (TextView) findViewById(R.id.link);
                     t.setText(link);
                     t = (TextView) findViewById(R.id.author);
-                    t.setText("HeHe!!!");
-
+                    t.setText(author_name);
+                    System.out.println("I'm getting the event's detail!!!");
 
 //                    GridView gridview = (GridView) findViewById(R.id.gridview);
 //                    //              if (imageURLs.size()>0){
@@ -159,7 +166,7 @@ public class DisplayOneEvent extends ActionBarActivity {
 //                    });
 
                 } catch (Exception e) {
-                    System.out.println("Exception");
+                    System.out.println("!!!Got an exception!!!" + e.toString());
                 }
 
 
@@ -173,41 +180,41 @@ public class DisplayOneEvent extends ActionBarActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.display_mysubscribe, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.display_mysubscribe, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
-
-    public  void imageUpload(View view){
-        Intent intent = new Intent(this,ImageUpload.class);
-
-        intent.putExtra(EXTRA_MESSAGE,msg);
-
-        startActivity(intent);
-    }
-
-    public void viewAllImages(View view){
-        Intent intent= new Intent(this, DisplayImages.class);
-        //if(Homepage.login)
-        String[] msg_out = new String[3];
-        msg_out[0] = email;
-        intent.putExtra(EXTRA_MESSAGE,msg_out);
-        startActivity(intent);
-    }
+//    public  void imageUpload(View view){
+//        Intent intent = new Intent(this,ImageUpload.class);
+//
+//        intent.putExtra(EXTRA_MESSAGE,msg);
+//
+//        startActivity(intent);
+//    }
+//
+//    public void viewAllImages(View view){
+//        Intent intent= new Intent(this, DisplayImages.class);
+//        //if(Homepage.login)
+//        String[] msg_out = new String[3];
+//        msg_out[0] = email;
+//        intent.putExtra(EXTRA_MESSAGE,msg_out);
+//        startActivity(intent);
+//    }
 
 }
